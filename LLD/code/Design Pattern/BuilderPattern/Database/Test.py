0eb127e5bfbf4ea2590a5c1374b6d651e9e81e4f
rafiqul.islam <rafiqulislam.mca18.du@gmail.com>
2023-02-07 03:25:57 +0530

    yes we have add new button

--- LLD/code/Design Pattern/BuilderPattern/Database/Test.py
+++ LLD/code/Design Pattern/BuilderPattern/Database/Test.py
@@ -1,5 +1,6 @@
 import unittest
-from  datastore import DataStore
+from .datastore import DataStore
+from .DB import Database
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
@@ -11,14 +12,42 @@ class MyTestCase(unittest.TestCase):
         #b=a.sethostname("localhost").set_port("80").set_password("password").build()
         print(a.__str__())
         assert isinstance(a,DataStore)
+
+
+
+    def test_database_(self):
+        map={}
+        map["host"]= "localhost"
+        map["port"]=12345
+        map["database"]="bd"
+        map["password"]="password"
+        a=Database(**map)
+        print(map)
+        assert isinstance(a,Database)
+
+
+    def test_database_builder(self):
+        #b=DataStore.DataStorebUILDER("sql",123,12346)
+        #a=DataStore.DataStorebUILDER.build(b)
+
+        assert  isinstance(True,bool)
+
     def test_database_abd(self):
-        a=DataStore.DataStoreBuilder
-
-        a.sethostname("localhost").build()
-
-
+        a=DataStore.DataStoreBuilder().sethostname("h").set_password("password").set_port("port").build()
 
         assert isinstance(a,DataStore)
+
+    def test__database(self):
+        map={}
+        map["host"]= "localhost"
+        map["port"]=12345
+        map["database"]="bd"
+        map["password"]="password"
+        a=Database(**map)
+        assert  isinstance(a,Database)
+
+
+
 
 # add assertion here
 
